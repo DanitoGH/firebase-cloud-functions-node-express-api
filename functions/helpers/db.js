a6e81217da9dba399abc3099d2b706bc4994e2b4
DANIEL ADJEI BOATENG <freshdan24@gmail.com>
2022-07-31 22:44:51 +0000

    mongodb connection removed

--- functions/helpers/db.js
+++ functions/helpers/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
-const DB_CONNECTION_STRING = "mongodb+srv://test_database:xtonlBKyzPLeJhJs@cluster0.2gvqsrd.mongodb.net/test?retryWrites=true&w=majority"
+const DB_CONNECTION_STRING = ""
 
 const connectDB = async () => {
   try{
     const conn = await mongoose.connect(DB_CONNECTION_STRING, { useUnifiedTopology: true }, { useNewUrlParser: true })
-    console.log("Database connected...")
   } catch(error) {
     console.log(error)
   }
